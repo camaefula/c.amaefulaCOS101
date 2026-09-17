f4df1f41d0494fb63696910887142d229fc372de
unknown <chimsomaga.amaefula@pau.edu.ng>
2026-09-17 18:57:32 +0100

    submit week-4 practicals

---
+++ week-4/practice_9/target/debug/deps/practice_9-099876945606246f.d
@@ -0,0 +1,5 @@
+C:\Users\NEW\Desktop\COS101\c.amaefulaCOS101\week-4\practice_9\target\debug\deps\practice_9-099876945606246f.d: src\main.rs
+
+C:\Users\NEW\Desktop\COS101\c.amaefulaCOS101\week-4\practice_9\target\debug\deps\practice_9-099876945606246f.exe: src\main.rs
+
+src\main.rs:
